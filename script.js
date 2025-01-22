8c7192dfcec27272fe69fbd15821e5d498abffc1
amnaduny <amnaduniakhara@gmail.com>
2025-01-22 22:58:12 +0100

    remove all comments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Function to dynamically load the navbar from index.html
+
 function loadNavbar() {
     fetch('index.html')
         .then(response => response.text())
@@ -6,11 +6,11 @@ function loadNavbar() {
             const tempDiv = document.createElement('div');
             tempDiv.innerHTML = data;
             const navbar = tempDiv.querySelector('#navbar-section');
-            // document.getElementById('navbar').innerHTML = navbar.outerHTML;
-            document.body.insertAdjacentHTML('afterbegin', navbar.outerHTML); // adding the navbar by replacing the id="navbar"
+            
+            document.body.insertAdjacentHTML('afterbegin', navbar.outerHTML); 
         })
         .catch(error => console.error('Error loading navbar:', error));
 }
 
-// Ensure the navbar loads after the page content
+
 window.addEventListener('load', loadNavbar);
